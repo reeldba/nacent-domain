d2d2fb57cf1622129504647cf32a63a69a598a73
Allen Pippin <reeldba@gmail.com>
2016-12-19 00:13:38 +0000

    stopping for tonight

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# nacent-domain
-Quill-I-Am's automated habitat
+# nacent-domain -- Quill-I-Am's automated habitat
+
+## Purpose
+
+So my family keeps and raises pygmy hedgehogs.  
+Hedgies are warm weather critters and do best when their habitat is kept 
+around 74-78 degrees farenheit.   If it goes any lower than 72 the little 
+guys can start to go into a hibernation state that they are not really 
+capable of and this can lead to all kinds of bad things including your 
+beloved little pin-cushion dying.
+
+So this project seeks to automate the temperature of the habitat by measuring
+the temperature and then remotely turning on a heat lamp and / or a hot-spot
+warming spot fo the hedgie.
+
+If successful, this should (a) keep hedgie warm and happy year round, (b) pay
+for itself pretty quickly in reduced utility bills and (c) make my wife and
+daughter very happy.
+
+## Parts List
+
+* Raspberry Pi 3 Model B.  This is overkill - but overkill is under-rated.  I
+may enable the remote receptacles to wake up my teenager on weekdays.
+* Adafruit DHT-8302 Temperature and Humidity Sensor
+* SMAKN 433Mhz Receiver and Transmitter
+* Elektracity 5 Pack remote controlled outlets
+
+Note - I bought the Adafruit starter pack for this project.  Those guys rock.
+It arrived in no time.
+
+## Software
+
+I'm using these libraries, plus what I write
+
+* Adaafruit DHT Libraries
--- src/habitat.py
+++ src/habitat.py
@@ -1,15 +1,62 @@
+#!/usr/bin/python
+import gpiozero
 import RPi.GPIO as GPIO
 import datetime
 import Adafruit_DHT
 
+# NOTE - REPLACE THIS WITH A CONFIG JSON 
 SENSOR=Adafruit_DHT.AM2302
 SENSOR_PIN=23
 
+TARGET_TEMPERATURE=26.67
+LOWER_CONTROL_LIMIT=23.88
+UPPER_CONTROL_LIMIT=29.44
+LOWER_ALERT_LIMIT=19.00
+UPPER_ALERT_LIMIT=31.00
+
+
+# PUT A LOCK IN PLACE - DON'T LET A SECOND INSTANCE FIRE UP IF WE ARE 
+# STILL WAITING ON THIS INSTANCE TO FINISH
+#
+# lock me here
+#
+
+
+def record_observation(field1,field2,field3):
+	print('{0} Temp={1:0.1f}*C Humidity={2:0.1f}% {3}'.format(datetime.datetime.utcnow(),field1,field2,field3))
+
+# ===========================================================================
+# MAIN LINE OF CODE STARTS HERE
+# ===========================================================================
+
+# Read the sensors
 humidity,temperature = Adafruit_DHT.read_retry(SENSOR,SENSOR_PIN)
 
-if humidity is not None and temperature is not None:
-	print('{0} Temp={1:0.1f}*C Humidity={2:0.1f}%'.format(datetime.datetime.utcnow(),temperature,humidity))
-else:
-	print('{0} Temp=None Humidity=None'.format(datetime.datetime.utcnow()))
+if humidity is none or temperature is none:
+	# couldn't get a reading
+	record_observation(field1=temperature, field2=humidity, field3="failed to get a reading")
+
+
+else;
+
+	if temperature < TARGET_TEMPERATURE:
+		if temperature < LOWER_CONTROL_LIMIT:
+			rc=RemoteHeater(on)
+			if temperature < LOWER_ALERT_LIMIT:
+				# alert someone
+		else:
+
+
+	elif temperature > TARGET_TEMPERATURE:
+		if temperature > UPPER_CONTROL_LIMIT:
+			rc=remoteHeater(off)
+			if temperature > UPPER_CONTROL_LIMIT:
+				# alert someone
+	else:
+		# must have equaled it
+
+# ===========================================================================
+# clean up
+# ===========================================================================
 
 # vim: ts=4 number
